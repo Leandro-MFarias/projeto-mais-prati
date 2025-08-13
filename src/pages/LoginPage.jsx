1ba732c977ae83698c75751356c15f3650d3ba57
Leandro Farias <leandrorf1606@gmail.com>
2025-08-13 16:24:31 -0300

    style: loginPage responsive

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,37 +2,37 @@ import { Link } from "react-router";
 
 export function LoginPage() {
   return (
-    <div className="grid h-screen grid-cols-2 bg-zinc-800 text-zinc-300">
+    <div className="relative h-screen md:h-auto text-zinc-300 md:static md:grid md:grid-cols-2 md:bg-zinc-800">
       {/* CONTAINER IMAGE */}
-      <div className="relative flex items-center justify-center bg-[url(/code.png)] bg-no-repeat">
-        <div className="absolute inset-0 bg-black/70"></div>
+      <div className="absolute -z-10 md:z-0 flex h-screen items-center justify-center bg-[url(/code.png)] bg-no-repeat md:relative">
+        <div className="absolute inset-0 bg-black/90 md:bg-black/70"></div>
 
-        <h1 className="relative text-center text-4xl font-semibold text-white">
+        <h1 className="invisible relative md:visible text-center text-4xl font-semibold md:text-white">
           Aprenda programação de forma leve, divertida e viciante
         </h1>
       </div>
 
       {/* CONTAINER FORM  */}
-      <div className="flex w-full flex-col items-center justify-center space-y-4">
+      <div className="flex h-screen w-full flex-col items-center justify-center space-y-4">
         <h1 className="text-3xl font-bold text-purple-600">LOGO AQUI</h1>
-        <form className="flex flex-col items-center space-y-6">
-          <div className="flex flex-col space-y-2">
+        <form className="flex w-[70%] flex-col items-center space-y-6 xl:w-[55%]">
+          <div className="flex w-full flex-col space-y-2">
             <label htmlFor="email" className="text-sm font-extralight">
               E-mail
             </label>
             <input
               type="text"
-              className="w-[520px] rounded-md border-2 border-neutral-900 bg-neutral-950 px-4 py-3 outline-none focus:border-purple-600"
+              className="rounded-md border-2 border-neutral-900 bg-neutral-950 px-4 py-3 outline-none focus:border-purple-600"
             />
           </div>
 
-          <div className="flex flex-col space-y-2">
+          <div className="flex w-full flex-col space-y-2">
             <label htmlFor="email" className="text-sm font-extralight">
               Senha
             </label>
             <input
               type="text"
-              className="w-[520px] rounded-md border-2 border-neutral-900 bg-neutral-950 px-4 py-3 outline-none focus:border-purple-600"
+              className="rounded-md border-2 border-neutral-900 bg-neutral-950 px-4 py-3 outline-none focus:border-purple-600"
             />
           </div>
 
@@ -43,12 +43,12 @@ export function LoginPage() {
             </label>
           </div>
 
-          <button className="w-[70%] cursor-pointer rounded-xl bg-purple-700 py-3 font-semibold transition duration-150 ease-in hover:bg-purple-600">
+          <button className="w-[80%] cursor-pointer rounded-xl bg-purple-700 py-3 font-semibold transition duration-150 ease-in hover:bg-purple-600">
             ENTRAR
           </button>
         </form>
         <Link to={"/register"}>
-          <p>
+          <p className="text-center">
             Não possui cadastro?
             <span className="font-semibold text-purple-500"> CADASTRE-SE</span>
           </p>
