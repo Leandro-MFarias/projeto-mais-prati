8f4214412013c4684def4bc0d511206a54436881
Leandro Farias <leandrorf1606@gmail.com>
2025-08-14 21:18:43 -0300

    style: add animate arrow on buttons

--- src/_components/journey.jsx
+++ src/_components/journey.jsx
@@ -1,6 +1,7 @@
 // import { Star } from "lucide-react";
 import { FaStar } from "react-icons/fa6";
 import exercises from "../service/exercises.json";
+import { MessageCircle, MessageSquare, MessageSquareText } from "lucide-react";
 
 export function Journey() {
   return (
@@ -8,7 +9,9 @@ export function Journey() {
       {/* HEADER OF SECTION */}
       <div className="flex w-full items-center space-x-4 px-16">
         <div className="h-[1px] w-full bg-neutral-600" />
-        <p className="text-xl font-bold">Introdução</p>
+        <p className="w-full text-center text-xl font-bold">
+          Lógica de Programação
+        </p>
         <div className="h-[1px] w-full bg-neutral-600" />
       </div>
 
@@ -17,15 +20,20 @@ export function Journey() {
         {exercises.map((exerciseGroup) => (
           <div
             key={exerciseGroup[0].id}
-            className="flex flex-col items-center space-y-3"
+            className="flex flex-col items-center space-y-4.5"
           >
-            <div>
+            <div className="relative">
               <button
-                className={`btn-exercise relative flex h-24 w-24 cursor-pointer items-center justify-center rounded-full ${exerciseGroup[0].completed === true ? "bg-purple-700" : "bg-zinc-600"}`}
+                className={`relative flex h-24 w-24 cursor-pointer items-center justify-center rounded-full ${exerciseGroup[0].completed === true ? "btn-todo bg-purple-600" : "btn-block bg-zinc-600"}`}
               >
                 <FaStar className="text-2xl" />
-                {/* <div className="absolute bottom-0 h-10 w-10 bg-zinc-100" /> */}
               </button>
+              <div className="absolute">
+                <div className="relative inline-block translate-x-2 -translate-y-32 animate-bounce rounded border border-neutral-600/80 bg-[#252627] px-3 py-1 font-bold text-purple-500">
+                  START
+                  <div className="absolute -bottom-2 left-1/2 h-0 w-0 -translate-x-1/2 transform border-t-6 border-r-6 border-l-6 border-t-neutral-100 border-r-transparent border-l-transparent"></div>
+                </div>
+              </div>
             </div>
             <div className="h-4 w-2 rounded-full bg-zinc-500" />
             <div className="h-4 w-2 rounded-full bg-zinc-500" />
