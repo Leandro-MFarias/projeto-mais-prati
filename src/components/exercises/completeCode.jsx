17cbbec4c62381445886d5b06d1a2c8a31a60c37
Leandro Farias <leandrorf1606@gmail.com>
2025-08-21 17:00:35 -0300

    feat: navigation to exercises

--- src/components/exercises/completeCode.jsx
+++ src/components/exercises/completeCode.jsx
@@ -1,11 +1,14 @@
 import { IoClose } from "react-icons/io5";
 import trophy from "../../assets/trophy.svg";
+import { Link } from "react-router";
 export function CompleteCode() {
   return (
     <div className="m-auto flex min-h-screen w-4xl flex-col space-y-16 py-9">
       {/* Barra */}
       <div className="flex w-4xl items-center justify-between gap-16">
-        <IoClose className="text-6xl"/>
+        <Link to={"/"}>
+          <IoClose className="text-5xl transition duration-150 ease-in hover:text-white" />
+        </Link>
         <div class="h-4 w-full rounded-full bg-zinc-600 dark:bg-gray-700">
           <div
             class="h-4 rounded-full bg-lime-600 dark:bg-blue-500"
@@ -24,19 +27,20 @@ export function CompleteCode() {
               as duas condições forem verdadeiras.
             </p>
             <code className="space-y-3">
-              <div className="flex space-x-2 items-center">
-                <div className="h-1 w-1 bg-zinc-100 rounded-full" />
-                
-                <p>let temConvite = <span className="text-lime-500">true</span></p>
+              <div className="flex items-center space-x-2">
+                <div className="h-1 w-1 rounded-full bg-zinc-100" />
+
+                <p>
+                  let temConvite = <span className="text-lime-500">true</span>
+                </p>
               </div>
-              <div className="flex space-x-2 items-center">
-                <div className="h-1 w-1 bg-zinc-100 rounded-full"/>
-                
-                <p>let nomeNaLista = <span className="text-red-600">false</span></p>
+              <div className="flex items-center space-x-2">
+                <div className="h-1 w-1 rounded-full bg-zinc-100" />
+
+                <p>
+                  let nomeNaLista = <span className="text-red-600">false</span>
+                </p>
               </div>
-              
-                
-              
             </code>
           </div>
         </div>
--- src/components/journey.jsx
+++ src/components/journey.jsx
@@ -1,9 +1,10 @@
 import { FaStar } from "react-icons/fa6";
 import exercises from "../service/exercises.json";
+import { Link } from "react-router";
 
 export function Journey() {
   return (
-    <div className="overflow-y-auto flex flex-1 flex-col items-center space-y-20 py-10">
+    <div className="flex flex-1 flex-col items-center space-y-20 overflow-y-auto py-10">
       {/* HEADER OF SECTION */}
       <div className="flex w-full items-center space-x-4 px-16">
         <div className="h-[1px] w-full bg-neutral-600" />
@@ -16,29 +17,28 @@ export function Journey() {
       {/* BLOCKS OF EXERCISE */}
       <div className="space-y-2">
         {exercises.map((exerciseGroup) => (
-          <div
-            key={exerciseGroup[0].id}
-            className="flex flex-col items-center space-y-4.5"
-          >
-            <div className="relative">
-              <button
-                className={`btn relative flex h-24 w-24 cursor-pointer items-center justify-center rounded-[50%] ${exerciseGroup[0].completed === true ? "completed bg-lime-500" : "blocked bg-zinc-600"}`}
-              >
-                <FaStar className="text-4xl" />
-              </button>
+          <Link to={"/exercises"} key={exerciseGroup[0].id}>
+            <div className="flex flex-col items-center space-y-4.5">
+              <div className="relative">
+                <button
+                  className={`btn relative flex h-24 w-24 cursor-pointer items-center justify-center rounded-[50%] ${exerciseGroup[0].completed === true ? "completed bg-lime-500" : "blocked bg-zinc-600"}`}
+                >
+                  <FaStar className="text-4xl" />
+                </button>
 
-              {/* START */}
-              <div className="absolute">
-                <div className="relative inline-block translate-x-2 -translate-y-32 animate-bounce rounded border border-neutral-600/80 bg-[#252627] px-3 py-1 font-bold text-purple-500">
-                  START
-                  <div className="absolute -bottom-2 left-1/2 h-0 w-0 -translate-x-1/2 transform border-t-6 border-r-6 border-l-6 border-t-neutral-100 border-r-transparent border-l-transparent"></div>
+                {/* START */}
+                <div className="absolute">
+                  <div className="relative inline-block translate-x-2 -translate-y-32 animate-bounce rounded border border-neutral-600/80 bg-[#252627] px-3 py-1 font-bold text-purple-500">
+                    START
+                    <div className="absolute -bottom-2 left-1/2 h-0 w-0 -translate-x-1/2 transform border-t-6 border-r-6 border-l-6 border-t-neutral-100 border-r-transparent border-l-transparent"></div>
+                  </div>
                 </div>
               </div>
+              <div className="h-4 w-2 rounded-full bg-zinc-500" />
+              <div className="h-4 w-2 rounded-full bg-zinc-500" />
+              <div className="h-4 w-2 rounded-full bg-zinc-500" />
             </div>
-            <div className="h-4 w-2 rounded-full bg-zinc-500" />
-            <div className="h-4 w-2 rounded-full bg-zinc-500" />
-            <div className="h-4 w-2 rounded-full bg-zinc-500" />
-          </div>
+          </Link>
         ))}
       </div>
     </div>
